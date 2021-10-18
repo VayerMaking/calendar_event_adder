b73205a7dcd90ea75d001757d78cb62106a09e27
VayerMaking <36741716+VayerMaking@users.noreply.github.com>
2021-10-18 15:33:19 +0300

    code cleanup

--- subject_event_adder.py
+++ subject_event_adder.py
@@ -10,11 +10,8 @@ excel_file = 'timesheet_tues.xlsx'
 timesheet = pd.read_excel(excel_file)
 print(timesheet)
 
-for i in timesheet.index:
-    # print(timesheet['day'][i], timesheet['time'][i], timesheet['subject'][i])
-    
+for i in timesheet.index:    
     calendar = GoogleCalendar('martin.vayer@gmail.com', credentials_path='client_secret_251214619756-gli6hcekqmbv4rqqju0mn03lao4o5k87.apps.googleusercontent.com.json')
-    # start_time = float(timesheet['time'][i].split('-')[0].replace(':', '.'))
     start_time = timesheet['time'][i].split('-')[0]
     end_time = timesheet['time'][i].split('-')[1]
 
@@ -24,6 +21,7 @@ for i in timesheet.index:
     WEDNESDAY = D.today() + WE
     THURSDAY = D.today() + TH
     FRIDAY = D.today() + FR
+
     if day == "MO":
      DAY = MONDAY
     elif day == "TU":
@@ -35,17 +33,11 @@ for i in timesheet.index:
     elif day == "FR":
      DAY = FRIDAY
     
-    #print(int(start_time))
-    # print(type(start_time))
+
     start_time_hour = int(start_time[:2])
     start_time_minutes = int(start_time[3:])
     end_time_hour = int(end_time[:2])
     end_time_minutes = int(end_time[3:])
-    # print(str(start_time_hour) + " + " + str(start_time_minutes))
-
-    # print(str(end_time_hour) + " + " + str(end_time_minutes))
-    # print(start_time == '13 : 40')
-    #print(day == "MO")
     
     event = Event(
          str(timesheet['subject'][i]),
@@ -54,8 +46,8 @@ for i in timesheet.index:
          recurrence=[Recurrence.rule(freq=WEEKLY)]
          
      )
-    print(event)
+
     calendar.add_event(event)
 
-for event in calendar:
-    print(event)
+# for event in calendar:
+#     print(event)
